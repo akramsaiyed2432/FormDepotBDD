4ea9400160c25fd75a4ff9e12fa689eedb10438a
akramsaiyed2432 <radixdt.2432@gmail.com>
2024-06-25 14:41:19 +0530

    UPdated multiple things

--- Desktop/FormDepotAdminPanelBDD/src/test/java/StepDef/TestNGCucumberRunner.java
+++ Desktop/FormDepotAdminPanelBDD/src/test/java/StepDef/TestNGCucumberRunner.java
@@ -1,12 +1,12 @@
 package StepDef;
 
-import org.junit.runner.RunWith;
+//import org.junit.runner.RunWith;
 
-import io.cucumber.junit.Cucumber;
+//import io.cucumber.junit.Cucumber;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@RunWith(Cucumber.class)
+//@RunWith(Cucumber.class)
 @CucumberOptions(
 		features="src/test/resources/Features", 
 		glue= {"StepDef"},
